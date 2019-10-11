778c483b84a25b5dae6ab6a2603aa793fb2d17c1
Andrei Muntean <andrei.muntean.dev@gmail.com>
2019-10-11 23:13:14 +0100

    Implement caching for thumbnails.

--- handlers/thumbnail.go
+++ handlers/thumbnail.go
@@ -2,13 +2,23 @@ package handlers
 
 import (
 	"../services/imageService"
+	"image"
 	"image/jpeg"
 	"net/http"
 	"strings"
 )
 
+var cachedThumbnails map[string]image.Image = make(map[string]image.Image)
+
+func getThumbnailForId(id string) image.Image {
+	if cachedThumbnails[id] == nil {
+		cachedThumbnails[id] = imageService.CreateThumbnailFromUrl("https://storage.googleapis.com/andreimuntean/" + id)
+	}
+
+	return cachedThumbnails[id]
+}
+
 func thumbnail(w http.ResponseWriter, r *http.Request) {
 	id := strings.Split(r.URL.Path, "/")[2]
-	img := imageService.CreateThumbnailFromUrl("https://storage.googleapis.com/andreimuntean/" + id)
-	jpeg.Encode(w, img, nil)
+	jpeg.Encode(w, getThumbnailForId(id), nil)
 }
