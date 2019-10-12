260c05658409807344e184cab2c701d410b32f1e
Andrei Muntean <andrei.muntean.dev@gmail.com>
2019-10-12 04:13:54 +0100

    Log cache errors.

--- services/cacheService/cacheService.go
+++ services/cacheService/cacheService.go
@@ -3,6 +3,7 @@ package cacheService
 import (
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/memcache"
+	"log"
 )
 
 // Add adds a byte array to the cache.
@@ -18,6 +19,7 @@ func Get(c context.Context, key string) []byte {
 	item, err := memcache.Get(c, key)
 
 	if err != nil {
+		log.Print(err)
 		return nil
 	}
 
